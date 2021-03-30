15632f7d667b0803f630af6a9d5948a4c525adb0
dpz <1019355587@qq.com>
2021-03-30 17:26:35 +0800

    commit

--- zzzfyrw-site/vite.config.js
+++ zzzfyrw-site/vite.config.js
@@ -1,28 +1,70 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
-
 
 const dev_config = {
 
-  server: {port:3005},
-  test:"test",
-  base:"/",
+  publicDir:'public',
+  server: {
+    host:'0.0.0.0',
+    strictPort:true,
+    port:3005,
+    cors: true,
+    proxy:{
+
+
+    },
+  },
+  build:{
+    outDir: 'dist',
+    assetsDir: './src/assets',
+    assetsInlineLimit: 4096,
+    sourcemap: true,
+    minify:'terser',
+    brotliSize: true,
+  },
+  base:'/',
+  logLevel:'info',
+  plugins: [
+      vue(),
+  ]
+
+}
+
+
+const prd_config = {
+
+  publicDir:'public',
+  server: {
+    host:'0.0.0.0',
+    strictPort:true,
+    port:3005,
+    cors: true,
+    proxy:{
+
+
+    },
+  },
+  build:{
+    outDir: 'dist',
+    assetsDir: './src/assets',
+    assetsInlineLimit: 4096,
+    sourcemap: true,
+    minify:'terser',
+    brotliSize: true,
+  },
+  base:'/',
+  logLevel:'info',
   plugins: [vue()]
+
 }
 
 
 
+
 export default ({ command, mode }) => {
-  console.log("========")
-  console.log(command,mode)
-  console.log("========")
-  if (command === 'serve') {
+  if (command === 'serve' && mode === 'dev') {
     return dev_config
-  } else {
-    return {
-      // build specific config
-    }
+  } else if(mode === 'prd-line'){
+    return prd_config
   }
 }
