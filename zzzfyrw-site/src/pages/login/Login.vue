b1dcf56b8907e720d9219949d9470970c569128d
dpz <1019355587@qq.com>
2021-03-31 23:59:22 +0800

    login/Login.scss

--- zzzfyrw-site/src/pages/login/Login.vue
+++ zzzfyrw-site/src/pages/login/Login.vue
@@ -2,19 +2,29 @@
 
   <div class="login-container">
     <div class="login-header">
-      <span>联系我们</span>
-      <span>联系我们</span>
-      <span>联系我们</span>
-      <span>联系我们</span>
+      <el-space wrap :size="size">
+        <el-button type="text">联系我们</el-button>
+        <el-button type="text">联系我们</el-button>
+        <el-button type="text">联系我们</el-button>
+<!--        <el-button type="text">联系我们</el-button>-->
+
+      </el-space>
+
     </div>
     <div class="login-main">
       <div class="login-center">
         <div class="login-center-left" />
         <div class="login-center-right">
-          <p>hhhhhhhhhhh</p>
-          <p>hhhhhhhhhhh</p>
-          <p>hhhhhhhhhhh</p>
-          <p>hhhhhhhhhhh</p>
+          <div class="login-info">
+            <el-container>
+              <el-header class="login-info-header">
+                <span>Z-Admin后台管理系统</span>
+              </el-header>
+              <el-divider></el-divider>
+              <el-main></el-main>
+              <el-footer></el-footer>
+            </el-container>
+          </div>
         </div>
       </div>
     </div>
@@ -38,7 +48,12 @@
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      size: 20,
+    }
+  },
 }
 </script>
 
